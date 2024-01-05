ba30e6914e73219f97bbba20c7a66c7045807791
Ricm63 <ricardomoncada96@gmail.com>
2024-01-05 16:11:40 -0300

    repuestos home2.0

---
+++ guardarDatos.js
@@ -0,0 +1,12 @@
+function guardarDatos() {
+    const marca = document.getElementById('marca').value;
+    const modelo = document.getElementById('modelo').value;
+    const ano = document.getElementById('ano').value;
+    const motor = document.getElementById('motor').value;
+
+    // Aquí puedes procesar o almacenar los datos como desees
+    console.log("Marca:", marca);
+    console.log("Modelo:", modelo);
+    console.log("Año:", ano);
+    console.log("Motor:", motor);
+  }
