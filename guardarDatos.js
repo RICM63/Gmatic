569d8a7646bd49552d73ec86bee542139f15d653
ricm63 <ricardomoncada@gmail.com>
2024-01-14 22:57:53 -0300

    formulario

--- guardarDatos.js
+++ guardarDatos.js
@@ -1,12 +1,33 @@
-function guardarDatos() {
-    const marca = document.getElementById('marca').value;
-    const modelo = document.getElementById('modelo').value;
-    const ano = document.getElementById('ano').value;
-    const motor = document.getElementById('motor').value;
+function guardarYMostrarDatos() {
+  const marca = document.getElementById('marca').value;
+  const modelo = document.getElementById('modelo').value;
+  const ano = document.getElementById('ano').value;
+  const motor = document.getElementById('motor').value;
 
-    // Aquí puedes procesar o almacenar los datos como desees
-    console.log("Marca:", marca);
-    console.log("Modelo:", modelo);
-    console.log("Año:", ano);
-    console.log("Motor:", motor);
+  if (marca && modelo && ano  && motor ) {
+    // Crear un nuevo elemento de lista
+    const nuevaLista = document.getElementById('listaDatos');
+    const nuevoItem = document.createElement('li');
+   
+    nuevoItem.textContent = ` Marca: ${marca},Modelo: ${modelo}, Año: ${ano}, Motor: ${motor}`;
+    nuevaLista.appendChild(nuevoItem);
+
+    document.getElementById('marca').value = '';
+    document.getElementById('modelo').value = '';
+    document.getElementById('ano').value = '';
+    document.getElementById('motor').value = '';
+
+    document.getElementById('formulario').style.display='block';
+
+    document.getElementById('datosMostrados').style.display ='block';
+
+   
+    document.getElementById('formulario').style.display = 'none';
+  } else {
+    alert('Por favor, complete todos los campos.');
   }
+}
+
+
+  
+ 
